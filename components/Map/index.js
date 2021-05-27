afbfe2aedb3e21306ea1965ee7b6db1010e1f69f
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-05-26 17:33:24 -0700

    cleaned up collision zone business

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,12 +1,12 @@
-import React, { useCallback, useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { rooms } from "../../config";
 import { MapContext } from "../../contexts";
-import { useClientRect } from "../../hooks";
 import { arraysAreEqual } from "../../utils";
 import styles from "./map.module.css";
 
 const Map = ({ children, room, objectsRef, clearObjectsRef }) => { // objectsRef should be array of elements
   const [ready, setReady] = useState(false);
+  const { setCollisionZones } = useContext(MapContext);
   const objectsList = rooms[room]?.objects;
   const mapObjects = objectsList.map(obj => {
     return <MapObject {...{
@@ -16,6 +16,7 @@ const Map = ({ children, room, objectsRef, clearObjectsRef }) => { // objectsRef
   });
   useEffect(() => {
     clearObjectsRef();
+    setCollisionZones({});
     setReady(true);
   }, [room]);
   useEffect(() => {
@@ -37,61 +38,67 @@ const Map = ({ children, room, objectsRef, clearObjectsRef }) => { // objectsRef
   );
 }
 
+const mapObjectConfig = {
+  townhall: [70, 300, 0.3], // [top, left, collision zone height relative to object]
+  mossyhouse: [170, 60, 0.6],
+  wishingwell: [250, 350, 0.4],
+  witchshack: [50, 351, 0.2]
+}
+
 const MapObject = ({ name }) => {
   const [rect, setRect] = useState(null);
   const [zIndex, setZIndex] = useState(-1);
-  const [collisionZone, setCollisionZone] = useState(null);
-  const { setCollisionZones } = useContext(MapContext);
   const objectRef = useRef(null);
-  const config = {
-    townhall: [70, 300, 0.3], // [top, left, collision zone height relative to object]
-    mossyhouse: [170, 60, 0.2],
-    wishingwell: [250, 350, 0.4],
-    witchshack: [50, 351, 0.2]
-  }
   useEffect(() => {
-    if (!config[name] || !rect) return;
-    setZIndex(Math.round(config[name][0] + rect.height - 12)); // 12 is approx half of avatar height but maybe todo better
+    if (!mapObjectConfig[name] || !rect) return;
+    setZIndex(Math.round(mapObjectConfig[name][0] + rect.height - 12)); // 12 is approx half of avatar height but maybe todo better
   }, [rect]);
+  if (!mapObjectConfig[name]) return null;
+  return (
+    <>
+      <img src={`/assets/map/${name}.png`} style={{
+        top: `${mapObjectConfig[name][0]}px`,
+        left: `${mapObjectConfig[name][1]}px`,
+        zIndex: `${zIndex}`
+      }} ref={objectRef} onLoad={() => setRect(objectRef.current?.getBoundingClientRect())} />
+      {rect?.height && <CollisionZone {...{ name, object: objectRef.current, rect }} />}
+    </>
+  );
+}
+
+const CollisionZone = ({ name, object, rect }) => {
+  const [{ top, left, width, height }, setRect] = useState({});
+  const collisionZoneRef = useRef(null);
+  const { setCollisionZones } = useContext(MapContext);
   useEffect(() => {
-    if (!rect?.height) return;
-    let { top: objectTop, left: objectLeft } = objectRef.current?.style;
+    let { top: objectTop, left: objectLeft } = object?.style;
     const { width: objectWidth, height: objectHeight } = rect;
     objectTop = parseInt(objectTop);
     objectLeft = parseInt(objectLeft);
-    const heightFactor = config[name][2];
-    const [top, left, width, height] = [
-      objectTop + (objectHeight * (1 - heightFactor)),
-      objectLeft,
-      objectWidth,
-      objectHeight * heightFactor
-    ]
-    const createReference = (element) => {
+    const heightFactor = mapObjectConfig[name][2];
+    setRect({
+      top: objectTop + (objectHeight * (1 - heightFactor)),
+      left: objectLeft,
+      width: objectWidth,
+      height: objectHeight * heightFactor
+    });
+  }, []);
+  useEffect(() => {
+    const element = collisionZoneRef.current;
+    if (element) {
       setCollisionZones(prevZones => ({
         ...prevZones,
         [name]: element
       }));
     }
-    const collisionZoneElement = (
-      <div className={styles.collision} style={{
-        top: `${top}px`,
-        left: `${left}px`,
-        width: `${width}px`,
-        height: `${height}px`
-      }} ref={createReference} />
-    );
-    setCollisionZone(collisionZoneElement);
-  }, [rect?.height]);
-  if (!config[name]) return null;
+  }, [collisionZoneRef.current]);
   return (
-    <>
-      <img src={`/assets/map/${name}.png`} style={{
-        top: `${config[name][0]}px`,
-        left: `${config[name][1]}px`,
-        zIndex: `${zIndex}`
-      }} ref={objectRef} onLoad={() => setRect(objectRef.current?.getBoundingClientRect())} />
-      {collisionZone}
-    </>
+    <div className={styles.collision} style={{
+      top: `${top}px`,
+      left: `${left}px`,
+      width: `${width}px`,
+      height: `${height}px`
+    }} ref={collisionZoneRef} />
   );
 }
 
--- contexts/MapContext.js
+++ contexts/MapContext.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
 export const MapContext = React.createContext();
 
